e66ed61f7654c2d1ed8efcae2320ed362025a2d2
BY <devblossom77@gmail.com>
2020-12-01 23:35:35 +0900

    tuiEditor => react-draft-wysigwyg으로 변경중

--- src/redux/data/saga.js
+++ src/redux/data/saga.js
@@ -1,13 +1,20 @@
 import { takeLatest, call, put, delay, spawn } from "@redux-saga/core/effects";
 import { Action } from "./redux";
 import { dataCreators } from "../actionCreators";
+import { getAccessToken } from "../../helper/localStorageHelper";
+import { Api } from "../../api/editor";
 
 export default function* () {
-  // yield takeLatest(Action.Types.SET_TOAST_MESSAGE, function* ({message}) {
-  //   yield put(appCreators.updateState({toastMessage: message}))
-  //   yield spawn(function* () {
-  //       yield delay(4000);
-  //       yield put(appCreators.updateState({toastMessage: ''}))
-  //   })
-  // });
+  yield takeLatest(Action.Types.CREACT_NOTICE, function* ({ data }) {
+    const token = getAccessToken();
+    const result = yield call(Api.createNotice, token, data);
+
+    // 할것
+    // if (result?.res === "success") {
+    //   alert("등록 성공");
+    //   navigate("/community/notice");
+    // } else {
+    //   alert("등록 실패");
+    // }
+  });
 }
