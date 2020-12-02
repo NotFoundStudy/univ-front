24c350d69de02baad4eb5138dd52678ee23ec298
BY <devblossom77@gmail.com>
2020-12-02 22:45:10 +0900

    Home) 게시판 레이아웃

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -15,11 +15,16 @@ const Home = (props) => {
           <div className="board">
             <div className="name">
               <span>Notice</span>
-              <BsPlusSquare />
+              <BsPlusSquare
+                style={{ cursor: "pointer" }}
+                onClick={() => navigate("/community/notice")}
+              />
             </div>
             <div className="row">
               <div className="title">
-                반도체 소재연구 분야 전문가’, 이진균 고분…
+                반도체 소재연구 분야 전문가’, 이진균 고분 소재연구 분야 전문가’,
+                이진균 고분 소재연구 분야 전문가’, 이진균 고분 소재연구 분야
+                전문가’, 이진균 고분 소재연구 분야 전문가’, 이진균 고분
               </div>
               <div className="date">20.10.20</div>
             </div>
@@ -93,26 +98,43 @@ const Wrapper = styled.div`
       margin-right: -15px;
     }
     .board-wrap {
-      flex-grow: 1;
+      flex: 0 0 50%; // 이거와 maxWidth 같이줘야 50% 채워짐
+      max-width: 50%;
       padding: 0 15px;
     }
     .board {
+      min-height: 251px;
       box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
       background: #fff;
       .name {
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding: 10px 5px;
+        padding: 12px 17px 9px 20px;
         border-bottom: 2px solid #999;
         font-size: 21px;
         font-weight: 500;
         color: #525252;
       }
       .row {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 15px 12px 11px 13px;
+        & + .row {
+          border-top: 1px solid #ddd;
+        }
         .title {
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          padding-right: 60px;
+          font-size: 14px;
         }
         .date {
+          flex-shrink: 0;
+          font-size: 12px;
+          opacity: 0.7;
         }
         .thumb {
           img {
