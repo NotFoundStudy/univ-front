d470cfc2f1427b404fe4e12434fd061f5404bf82
gorillaKim <yhkim@madup.com>
2021-02-01 20:36:12 +0900

    [feature] create mockup data

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,4 +1,50 @@
 
 export const mock = {
+    lecture: {
+        lists: [
+            {
+                id: 123123,
+                title: "This is example title!!!",
+                avatarUrl: "http://cdn.ppomppu.co.kr/zboard/data3/2016/1115/m_1479190334_2292_Patrick_Star.png",
+                content: "this is lecture, hey example!!! hey exmple!!! hey example!!! this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!",
+                publisher: "young-bin ko"
+            },
+            {
+                id: 123123,
+                title: "This is example title!!!",
+                avatarUrl: "http://cdn.ppomppu.co.kr/zboard/data3/2016/1115/m_1479190334_2292_Patrick_Star.png",
+                content: "this is lecture, hey example!!! hey exmple!!! hey example!!! this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!",
+                publisher: "young-bin ko"
+            },
+            {
+                id: 123123,
+                title: "This is example title!!!",
+                avatarUrl: "http://cdn.ppomppu.co.kr/zboard/data3/2016/1115/m_1479190334_2292_Patrick_Star.png",
+                content: "this is lecture, hey example!!! hey exmple!!! hey example!!! this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!",
+                publisher: "young-bin ko"
+            },
+            {
+                id: 123123,
+                title: "This is example title!!!",
+                avatarUrl: "http://cdn.ppomppu.co.kr/zboard/data3/2016/1115/m_1479190334_2292_Patrick_Star.png",
+                content: "this is lecture, hey example!!! hey exmple!!! hey example!!! this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!",
+                publisher: "young-bin ko"
+            },
+            {
+                id: 123123,
+                title: "This is example title!!!",
+                avatarUrl: "http://cdn.ppomppu.co.kr/zboard/data3/2016/1115/m_1479190334_2292_Patrick_Star.png",
+                content: "this is lecture, hey example!!! hey exmple!!! hey example!!! this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!",
+                publisher: "young-bin ko"
+            },
+            {
+                id: 123123,
+                title: "This is example title!!!",
+                avatarUrl: "http://cdn.ppomppu.co.kr/zboard/data3/2016/1115/m_1479190334_2292_Patrick_Star.png",
+                content: "this is lecture, hey example!!! hey exmple!!! hey example!!! this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!this is lecture, hey example!!! hey exmple!!! hey example!!!",
+                publisher: "young-bin ko"
+            }
 
+        ]
+    }
 };
