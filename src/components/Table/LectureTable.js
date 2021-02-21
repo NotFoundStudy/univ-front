4d63f26899b31b6fb2a108525218a4d5387eaed6
gorillaKim <yhkim@madup.com>
2021-02-21 13:44:38 +0900

    [feature] create Table component using antd

---
+++ src/components/Table/LectureTable.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+
+import { Table, Tag, Space } from 'antd';
+const { Column, ColumnGroup } = Table;
+
+const config = {
+  pagination : {
+    pageSizeOptions : ['5', '10'],
+    showSizeChanger : true
+  }
+}
+
+const LectureTable = ({data}) => {
+
+  return (
+    <Table dataSource={data} {...config}>
+      <Column title="Date" dataIndex="date" key="date" width="15%"/>
+      <ColumnGroup title="Author">
+        <Column title="Name" dataIndex="name" key="name" width="10%"/>
+        <Column title="Edu" dataIndex="education" key="education" width="5%"/>
+      </ColumnGroup>
+      <Column title="Title" dataIndex="title" key="title" width="40%"	/>
+      <Column
+        title="Tags"
+        dataIndex="tags"
+        key="tags"
+        width="20%"
+        render={tags => (
+          <>
+            {tags.map(tag => (
+              <Tag color="blue" key={tag}>
+                {tag}
+              </Tag>
+            ))}
+          </>
+        )}
+      />
+      <Column
+        title="Action"
+        key="action"
+        render={(text, record) => (
+          <Space size="middle">
+            <a href={record.pdfUrl}>PDF {record.lastName}</a>
+            <a>Delete</a>
+          </Space>
+        )}
+      />
+    </Table>
+  )
+};
+
+export default LectureTable;
